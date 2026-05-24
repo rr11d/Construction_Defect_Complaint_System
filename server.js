6587c3bd6b13e2c7ce8a338fb0e7ea882d5407a6
hummi <humming133@gmail.com>
2026-05-25 05:25:08 +0900

    Restore Docker database setup

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use('/uploads', express.static(UPLOAD_ROOT));
 // 2. MySQL 연결 설정
 const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT || 3306),
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
